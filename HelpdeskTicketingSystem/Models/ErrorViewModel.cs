353b098e5dc67fb2fb4b995de9675cdfcf79ce03
Ferdinand II Dangaran <161916848+Shanindagaas@users.noreply.github.com>
2025-05-17 00:07:16 +0800

    added basic mvc

---
+++ HelpdeskTicketingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelpdeskTicketingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
